a3a3281c8cb55600076b6ed9e8fcc4b768245945
Zigoval <rouby.v@hotmail.com>
2019-02-14 14:51:55 +0100

    feat/Q9

--- TodoList/Controllers/ChecklistViewController.swift
+++ TodoList/Controllers/ChecklistViewController.swift
@@ -9,24 +9,34 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var ArrayCheckListItem = [ChecklistItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ChecklistItem(text:"Finirlecoursd’iOS")
-        ChecklistItem(text:"MettreàjourXCode",checked:true)
+        ArrayCheckListItem.append(ChecklistItem(text:"Finir le cours d’iOS"))
+    ArrayCheckListItem.append(ChecklistItem(text:"Mettre à jour XCode",checked:true))
     }
     
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int{return 1}
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int{return ArrayCheckListItem.count}
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)-> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        cell.textLabel!.text = "WE ARE HERE TO STEAL YOUR BEER"
+        configureText(for: cell, withItem: ArrayCheckListItem[indexPath.row])
+        configureCheckmark(for: cell, withItem: ArrayCheckListItem[indexPath.row])
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, withItem item: ChecklistItem){
+        cell.accessoryType = item.checked ?  .checkmark : .none
+    }
+    func configureText(for cell: UITableViewCell, withItem item: ChecklistItem){
+        cell.textLabel?.text = item.text
     }
     
 }
