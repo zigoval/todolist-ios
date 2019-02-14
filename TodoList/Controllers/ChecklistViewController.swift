f89e920912df34145c19af9996cccd82edc939f9
Zigoval <rouby.v@hotmail.com>
2019-02-14 14:25:08 +0100

    feat/Q8

--- TodoList/Controllers/ChecklistViewController.swift
+++ TodoList/Controllers/ChecklistViewController.swift
@@ -25,6 +25,9 @@ class ChecklistViewController: UITableViewController {
         cell.textLabel!.text = "WE ARE HERE TO STEAL YOUR BEER"
         return cell
     }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
